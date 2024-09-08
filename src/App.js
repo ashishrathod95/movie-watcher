28b80deae2de8da8ee22125cd2704c810a709bee
ashishrathod95 <anusudha.1212@gmail.com>
2024-09-08 15:35:50 +0530

    Worked on App.js and added Banner section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import './App.css';
+import Row from './Row';
+import requests from './Requests'
+import Banner from './Banner';
 
 function App() {
   return (
-    <div className="App">
-        
+    <div className="app">
+      {/* Navbar */}
+
+      {/* Banner */}
+      <Banner />
+      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
+      <Row title="Animation" fetchURL={requests.fetchAnimation} />
     </div>
   );
 }
