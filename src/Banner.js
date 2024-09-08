b12f8fde961841911cf9a6dc9ee53bbc59f018ae
ashishrathod95 <anusudha.1212@gmail.com>
2024-09-08 23:44:36 +0530

    Added Banner section with navbar and banner

--- src/Banner.js
+++ src/Banner.js
@@ -16,6 +16,11 @@ function Banner() {
   }, []);
 
   // console.log(movie)
+
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
     <header className='banner' style={{
       backgroundSize: "cover",
@@ -23,7 +28,7 @@ function Banner() {
     }}> {/*Background Image*/}
       <div className="banner__contents">
         {/* title */}
-        <h1>
+        <h1 className='banner__title'>
           {movie?.title || movie?.name || movie?.original_name}
         </h1>
         {/* div > 2 buttons */}
@@ -32,7 +37,11 @@ function Banner() {
           <button className="banner__button">My List</button>
         </div>
         {/* description */}
+        <div className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </div>
       </div>
+      <div className="banner__fadeBottom"></div>
     </header>
   )
 }
